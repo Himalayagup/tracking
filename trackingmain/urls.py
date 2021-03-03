e2fdbfbb70d7918da0445153b0e73ad9ad3754bc
Himalaya <himalaya.gupta3@gmail.com>
2021-03-03 14:03:26 +0530

    'static'

--- trackingmain/urls.py
+++ trackingmain/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name="home"),
@@ -36,3 +37,4 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL,
                       document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
