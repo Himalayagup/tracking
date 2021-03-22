38705dfc60138b5294ab65ffe42be4b46e0004da
Himalaya <himalaya.gupta3@gmail.com>
2021-03-22 16:24:13 +0530

    Hitcount removed

--- trackingmain/settings.py
+++ trackingmain/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third party apps
-    'hitcount',
     'bootstrap4',
     'widget_tweaks',
     'mathfilters',
