e002f6357256fd5841f2ec822a4827c4e9a078b6
Himalaya Gupta <himalaya.gupta3@gmail.com>
2021-03-30 18:22:21 +0530

    session domains

--- trackingmain/settings.py
+++ trackingmain/settings.py
@@ -72,7 +72,7 @@ XS_SHARING_ALLOWED_METHODS = ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE']
 CORS_ORIGIN_ALLOW_ALL = True
 # Expering session when browser gets closed
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-
+SESSION_COOKIE_DOMAIN = "."
 ROOT_URLCONF = 'trackingmain.urls'
 
 TEMPLATES = [
