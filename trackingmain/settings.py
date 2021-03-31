5c481148a76de3b5479509d5b9566c5c24123dbf
Himalaya Gupta <himalaya.gupta3@gmail.com>
2021-03-31 12:20:59 +0530

    Debug True

--- trackingmain/settings.py
+++ trackingmain/settings.py
@@ -22,7 +22,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = '5(+t&q@@qp5#xb%qjcyyg$133p6!48sn+a7axlf43)8c%g!_8g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.29.16', '192.168.29.87', '127.0.0.1',
                  'localhost', '143.110.177.56', 'kagayaki.co.in', 'www.kagayaki.co.in']
